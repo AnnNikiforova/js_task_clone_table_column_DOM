fee924119103628e9111d429516548371eba5b5a
Anna <annanicky90gmail.com>
2024-10-17 14:00:51 +0400

    Copy/Paste table column

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,24 @@
 'use strict';
 
-// write your code here
+function cloneColumn(columnIndex) {
+  const headRows = table.querySelectorAll('thead tr');
+  const bodyRows = table.querySelectorAll('tbody tr');
+  const footRows = table.querySelectorAll('tfoot tr');
+
+  const insertClonedCell = (rows) => {
+    rows.forEach((row) => {
+      const cells = row.children;
+      const clonedCell = cells[columnIndex].cloneNode(true);
+
+      row.insertBefore(clonedCell, cells[cells.length - 1]);
+    });
+  };
+
+  insertClonedCell(headRows);
+  insertClonedCell(bodyRows);
+  insertClonedCell(footRows);
+}
+
+const table = document.querySelector('table');
+
+cloneColumn(table, 1);
