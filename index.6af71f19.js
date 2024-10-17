4bc3b071a6936fc841856d1b3fc78b893e3554f1
Anna <annanicky90gmail.com>
2024-10-17 14:36:06 +0400

    make build

---
+++ index.6af71f19.js
@@ -0,0 +1,2 @@
+!function(e,t){var r=e.querySelectorAll("thead tr"),o=e.querySelectorAll("tbody tr"),l=e.querySelectorAll("tfoot tr");function c(e){e.forEach(function(e){var t=e.children,r=t[1].cloneNode(!0);e.insertBefore(r,t[t.length-1])})}c(r),c(o),c(l)}(document.querySelector("table"),0);
+//# sourceMappingURL=index.6af71f19.js.map
