57ee0c61bfff9e0c2e1d25a49010c40c1897d953
Anna <annanicky90gmail.com>
2024-10-17 14:13:32 +0400

    adjust the function call

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,24 @@
 'use strict';
 
-function cloneColumn(columnIndex) {
-  const headRows = table.querySelectorAll('thead tr');
-  const bodyRows = table.querySelectorAll('tbody tr');
-  const footRows = table.querySelectorAll('tfoot tr');
+const employeeTable = document.querySelector('table');
 
-  const insertClonedCell = (rows) => {
+function cloneColumn(tableElement, columnIndex) {
+  const headRows = tableElement.querySelectorAll('thead tr');
+  const bodyRows = tableElement.querySelectorAll('tbody tr');
+  const footRows = tableElement.querySelectorAll('tfoot tr');
+
+  function insertClonedCell(rows) {
     rows.forEach((row) => {
       const cells = row.children;
       const clonedCell = cells[columnIndex].cloneNode(true);
 
       row.insertBefore(clonedCell, cells[cells.length - 1]);
     });
-  };
+  }
 
   insertClonedCell(headRows);
   insertClonedCell(bodyRows);
   insertClonedCell(footRows);
 }
 
-const table = document.querySelector('table');
-
-cloneColumn(table, 1);
+cloneColumn(employeeTable, 1);
